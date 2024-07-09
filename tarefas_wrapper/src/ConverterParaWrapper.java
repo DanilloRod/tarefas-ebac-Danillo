63b8ef1b9b234962b9f9095db9b48a19f445dc51
Danillo Rodrigues <danillorodrigues713@gmail.com>
2024-07-08 23:02:37 -0300

    entrega da tarefa do mod9

---
+++ tarefas_wrapper/src/ConverterParaWrapper.java
@@ -0,0 +1,14 @@
+/**
+ * @author danil
+ */
+
+public class ConverterParaWrapper {
+
+    public static void main(String[] args) {
+
+        int idade = 12;
+        System.out.println(idade);
+        Integer idade1 = idade;
+        System.out.println(idade1);
+    }
+}
